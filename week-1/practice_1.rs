1ee9394a88a3836c9dd2e2b0c3e41c7f8beff3e3
SST-LAB-1 <tobechukwu.ofili@pau.edu.ng>
2023-10-18 15:49:10 +0200

    (My First Commit)

---
+++ week-1/practice_1.rs
@@ -0,0 +1,9 @@
+fn man(){
+	println!("Welcome to COS 101")
+	prntln("The course learning outcome is as follows")
+	println!("1. Distinguish between computer and computer programming,/n
+	          2. Develop some technique in computer science,/n
+	          3. Understand the different areas of study in computer science,/n
+	          4. Navigate the career prospects in computer sciencem,/n
+	          6. Conversant with computer programming concepts")
+}
