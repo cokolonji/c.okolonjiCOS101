7fb5120708eb4fac2eef0b86ec94c7e479bd0398
cokolonji <148286406+cokolonji@users.noreply.github.com>
2023-10-19 22:42:53 +0100

    Add files via upload

---
+++ week-2/practice_2.rs
@@ -0,0 +1,11 @@
+fn main(){
+	println!(); // prints just a
+
+	println!("hello");//prints
+
+	println!("format {} arguments", "some"); //prints format some arguments
+
+	println!("My name is {}. I am class rep of stream 1", "Chioma Okolonji");
+
+
+}
---
+++ week-2/project_3.rs
@@ -0,0 +1,13 @@
+fn main() {
+	let p:f64 = 210000.0;
+	let r:f64 = 5.0;
+	let t:f64 = 3.0;
+
+	// Amount of the tv three years later
+	let a = p * (1.0 - (r / 100.0)).powf(t);
+	println!("Amount is {}", a);
+
+	// Depreciation
+	let d = a - p;
+	println!("Depreciation is {}", d);
+}
