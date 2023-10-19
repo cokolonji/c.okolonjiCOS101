aeadcefac802174f469405d345e6601ed0b3e439
cokolonji <148286406+cokolonji@users.noreply.github.com>
2023-10-19 12:18:03 +0100

    Add files via upload

---
+++ week-1/practice_1.rs
@@ -0,0 +1,11 @@
+fn main(){
+	println!("Wecome to COS 101")
+
+	println!("The course learning outcome is as follows:")
+
+	println!("1. Distinguish between computer and computer programming,/n
+	         2.Develop some techniques in computer science,/n
+	         3. understand the different area of study in computer science,/n
+	         4. navigate the computer prospects in computer science,/n
+	         5. conversant with computer programming concepts.")
+}
